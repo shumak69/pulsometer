0a833f3d39c9ced381dd3230308f47125558b69a
Dmitriy <dima06444@gmail.com>
2022-01-08 18:58:33 +0200

    seventh version

--- js/script.js
+++ js/script.js
@@ -59,5 +59,38 @@ document.querySelector('.next').addEventListener('click',  function () {
         $('.overlay, #order').fadeIn('slow');
       })
     });
+
+    function  validateForms(form){
+      $(form).validate({
+        rules:{
+            name:  {
+              required: true,
+              minlength: 2
+            },
+            phone: "required",
+            email: {
+              required: true,
+              email: true
+            }
+        },
+        messages: {
+          name: {
+            required: "Пожалуйста, введите свое имя",
+            minlength: jQuery.validator.format("Введите {0} символа!")
+          },
+          phone: "Пожалуйста, введите cвой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправильно введен адрес почты"
+          }
+        }
+      
+    });
+    }
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-9999");
 });
 })(jQuery);
